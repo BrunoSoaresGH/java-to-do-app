345fa1e52617a0490b9f5e2e1b7d920a29ed1e43
Bruno Soares <brunosoaresbsv@gmail.com>
2023-01-25 09:32:47 -0300

    Project To Do App created

---
+++ src/main/java/com/bs/todoapp/main/ToDoApp.java
@@ -0,0 +1,12 @@
+package com.bs.todoapp.main;
+
+/**
+ *
+ * @author Bruno Soares
+ */
+public class ToDoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
